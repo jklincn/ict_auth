a4dcf70bf20df1e733ec2cbfd855ca92ab519648
jklincn <jklincn@outlook.com>
2024-12-17 10:56:46 +0000

    add: ict_auth install script

---
+++ ict_auth.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# This is just a helper script to download ICT Auth.
+
+set -u
+
+err() {
+    echo "[ERROR]: $1" >&2
+    exit 1
+}
+
+# Download the installer
+echo "[INFO] Downloading ICT Auth installer..."
+if ! curl -f --progress-bar -o /tmp/ict_auth.run https://oss.jklincn.com/ict_auth.run; then
+    err "Failed to download installer"
+fi
+
+# Make it executable
+chmod +x /tmp/ict_auth.run
+
+# Run the installer with proper TTY handling
+if [ ! -t 0 ]; then
+    # Script is being piped, connect to TTY for interactive input
+    if [ ! -t 1 ]; then
+        err "Unable to run interactively"
+    fi
+
+    /tmp/ict_auth.run < /dev/tty
+else
+    # We already have a TTY
+    /tmp/ict_auth.run
+fi
+
+# Clean up
+rm -f /tmp/ict_auth.run
